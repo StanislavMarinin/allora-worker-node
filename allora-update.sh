97b69b13025986a4b161e1bc377ea8d8f766ad8b
STM <95878397+StanislavMarinin@users.noreply.github.com>
2024-08-15 19:24:15 +0500

    Create allora-update.sh

---
+++ allora-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+execute_with_prompt() {
+    echo -e "${BOLD}Executing: $1${RESET}"
+    if eval "$1"; then
+        echo "Command executed successfully."
+    else
+        echo -e "${BOLD}${DARK_YELLOW}Error executing command: $1${RESET}"
+        exit 1
+    fi
+}
+
+execute_with_prompt "cd ~/allora-chain/basic-coin-prediction-node"
+
+execute_with_prompt "docker compose down -v"
+execute_with_prompt "docker container prune"
+execute_with_prompt "cd ~/allora-chain/ && rm -rf basic-coin-prediction-node"
+execute_with_prompt "cd ~/allora-chain/ && git clone https://github.com/allora-network/basic-coin-prediction-node"
+execute_with_prompt "cd ~/allora-chain/basic-coin-prediction-node && wget config.json"
